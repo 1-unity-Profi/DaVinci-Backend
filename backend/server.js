bc4d651932a3d19fc8bf898a9dfbcda2305986d8
L11Wolkenkratzer <livioga2008@icloud.com>
2025-10-01 00:55:09 +0200

    Abgeändert damit Highscores überall funktioniert (#1)

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'http://localhost:5000',
     'http://localhost:5173',
-    'http://localhost:5174'
+   
   ],
   credentials: true
 }));
